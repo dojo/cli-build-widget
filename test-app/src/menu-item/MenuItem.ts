eef90b44407935f08ff740dade5ad067f108b79d
Matt Wistrand <matt@troglodytidae.com>
2018-01-17 09:25:50 -0600

    Add test-app

---
+++ test-app/src/menu-item/MenuItem.ts
@@ -0,0 +1,42 @@
+import { v } from '@dojo/widget-core/d';
+import { customElement } from '@dojo/widget-core/decorators/customElement';
+import { WidgetProperties } from '@dojo/widget-core/interfaces';
+import { theme, ThemedMixin } from '@dojo/widget-core/mixins/Themed';
+import { WidgetBase } from '@dojo/widget-core/WidgetBase';
+
+import * as css from './menuItem.m.css';
+
+export interface MenuItemProperties extends WidgetProperties {
+	title: string;
+	selected?: boolean;
+	data?: any;
+	onSelected?: (data: any) => void;
+}
+
+@customElement<MenuItemProperties>({
+	tag: 'demo-menu-item',
+	attributes: ['title', 'selected'],
+	events: ['onSelected'],
+	properties: ['data']
+})
+@theme(css)
+export class MenuItem extends ThemedMixin(WidgetBase)<MenuItemProperties> {
+	private _onClick() {
+		this.properties.onSelected && this.properties.onSelected(this.properties.data);
+	}
+
+	protected render() {
+		const { title, selected } = this.properties;
+
+		return v('li', { classes: this.theme(css.root) }, [
+			v(
+				'span',
+				{
+					classes: this.theme([css.item, selected ? css.selected : null]),
+					onclick: this._onClick
+				},
+				[title]
+			)
+		]);
+	}
+}
--- test-app/tests/functional/main.ts
+++ test-app/tests/functional/main.ts
@@ -1,11 +1,1 @@
-const { describe, it } = intern.getInterface('bdd');
-const { assert } = intern.getPlugin('chai');
-import App from '../../src/App';
-
-describe('functional test', () => {
-	it('my functional test', () => {
-		return App().then(foo => {
-			assert.strictEqual(foo(), 'foo');
-		});
-	});
-});
+/* Write your app tests here */
