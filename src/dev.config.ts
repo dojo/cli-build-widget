aba1ac976d096579c341e963e03db9d4ae22f25a
Bryan Forbes <bryan@reigndropsfall.net>
2018-06-08 10:53:24 -0500

    Set localIdentName on css-loader in dev mode
    Resolves #25

--- src/dev.config.ts
+++ src/dev.config.ts
@@ -5,10 +5,24 @@ import * as CleanWebpackPlugin from 'clean-webpack-plugin';
 
 function webpackConfig(args: any): webpack.Configuration {
 	const config = baseConfigFactory(args);
-	const { plugins, output } = config;
+	const { plugins, output, module } = config;
 	const location = path.join('dev', args.element.name);
 
 	config.plugins = [...plugins, new CleanWebpackPlugin([location], { root: output.path, verbose: false })];
+
+	module.rules = module.rules.map(rule => {
+		if (Array.isArray(rule.use)) {
+			rule.use.forEach((loader: any) => {
+				if (typeof loader === 'string') {
+					return;
+				}
+				if (loader.loader === 'css-loader') {
+					loader.options.localIdentName = '[name]__[local]__[hash:base64:5]';
+				}
+			});
+		}
+		return rule;
+	});
 
 	config.output = {
 		...output,
