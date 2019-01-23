4fe1da33f0df95ad67553c4e7b3b8dda7b5966c3
Matt Wistrand <matt.wistrand@gmail.com>
2019-01-23 09:53:53 -0600

    Change default feature to 'modern'

--- src/base.config.ts
+++ src/base.config.ts
@@ -89,7 +89,7 @@ export default function webpackConfigFactory(args: any): webpack.Configuration {
 	const jsonpIdent = args.element ? args.element.name : 'custom-elements';
 	const extensions = args.legacy ? ['.ts', '.tsx', '.js'] : ['.ts', '.tsx', '.mjs', '.js'];
 	const compilerOptions = args.legacy ? {} : { target: 'es6', module: 'esnext' };
-	const features = args.legacy ? args.features : { ...(args.features || {}), ...getFeatures('chrome') };
+	const features = args.legacy ? args.features : { ...(args.features || {}), ...getFeatures('modern') };
 
 	const postcssPresetConfig = {
 		browsers: args.legacy ? ['last 2 versions', 'ie >= 10'] : ['last 2 versions'],
