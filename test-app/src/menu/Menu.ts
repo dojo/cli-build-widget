8f42a9f95bbb1addce29480caab64705e05cbd5a
Bradley Maier <bmaier@sitepen.com>
2019-07-19 19:08:12 -0400

    Updates for custom element transformer

--- test-app/src/menu/Menu.ts
+++ test-app/src/menu/Menu.ts
@@ -1,61 +1,56 @@
-import has from '@dojo/framework/core/has';
-import { v } from '@dojo/framework/core/vdom';
-import { DNode, WidgetProperties, WNode } from '@dojo/framework/core/interfaces';
-import { theme, ThemedMixin } from '@dojo/framework/core/mixins/Themed';
-import { WidgetBase } from '@dojo/framework/core/WidgetBase';
-import { MenuItem, MenuItemProperties } from '../menu-item/MenuItem';
-
+import { v, create, isWNode, isVNode } from '@dojo/framework/core/vdom';
+import { WidgetProperties } from '@dojo/framework/core/interfaces';
+import { MenuItemProperties } from '../menu-item/MenuItem';
+import icache from '@dojo/framework/core/middleware/icache';
+import theme from '@dojo/framework/core/middleware/theme';
 import * as css from './menu.m.css';
+import has from '@dojo/framework/core/has';
 
 export interface MenuProperties extends WidgetProperties {
 	onSelected: (data: any) => void;
 }
 
-@theme(css)
-export class Menu extends ThemedMixin(WidgetBase)<MenuProperties, WNode<MenuItem>> {
-	private _selectedId: number | undefined;
+const render = create({ icache, theme }).properties<MenuProperties>();
 
-	private _onSelected(id: number, data: any) {
-		this._selectedId = id;
-		this.properties.onSelected(data);
-		this.invalidate();
-	}
-
-	protected render(): DNode {
-		const items = this.children.map((child, index) => {
-			if (child) {
-				const properties: Partial<MenuItemProperties> = {
-					onSelected: (data: any) => {
-						this._onSelected(index, data);
-					}
-				};
-				if (this._selectedId !== undefined) {
-					properties.selected = index === this._selectedId;
+export const Menu = render(({ properties, children, middleware: { icache, theme } }) => {
+	const { onSelected } = properties();
+	const themedCss = theme.classes(css);
+	const items = children().map((child, index) => {
+		if (isWNode(child) || isVNode(child)) {
+			const properties: Partial<MenuItemProperties> = {
+				onSelected: (data: any) => {
+					onSelected(data);
+					icache.set('selectedId', index);
 				}
-				child.properties = { ...child.properties, ...properties };
+			};
+			const selectedId = icache.get<number>('selectedId');
+			if (typeof selectedId !== 'undefined') {
+				properties.selected = index === selectedId;
 			}
-			return child;
-		});
-
-		const navAttributes: any = { classes: this.theme(css.root) };
-
-		if (has('foo')) {
-			navAttributes['data-foo'] = 'true';
-		}
-		if (has('bar')) {
-			navAttributes['data-bar'] = 'true';
+			child.properties = { ...child.properties, ...properties };
 		}
 
-		return v('nav', navAttributes, [
-			v(
-				'ol',
-				{
-					classes: this.theme(css.menuContainer)
-				},
-				items
-			)
-		]);
+		return child;
+	});
+
+	const navAttributes: any = { classes: themedCss.root };
+
+	if (has('foo')) {
+		navAttributes['data-foo'] = 'true';
 	}
-}
+	if (has('bar')) {
+		navAttributes['data-bar'] = 'true';
+	}
+
+	return v('nav', navAttributes, [
+		v(
+			'ol',
+			{
+				classes: themedCss.menuContainer
+			},
+			items
+		)
+	]);
+});
 
 export default Menu;
