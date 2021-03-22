8bf04cbaa2cc889f5be1a6216c9923824f9a5289
Anthony Gubler <agubler@sitepen.com>
2021-03-22 12:21:56 +0000

    add support for namespace

--- src/template/custom-element.js
+++ src/template/custom-element.js
@@ -1,9 +1,11 @@
 var registerCustomElement = require('@dojo/framework/core/registerCustomElement').default;
 
 var entryName = __ENTRY__;
+var namespace = null;
 var entry = document.querySelector(`script[src*="${entryName}"]`);
 if (entry) {
 	__webpack_public_path__ = entry.src.replace(new RegExp(`${entryName}.*`), '');
+	namespace = entry.getAttribute('namespace');
 }
 
 function useDefault(p) {
